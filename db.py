6fbf4384be1d639a88e575cb4235ebd5ed07b296
lcch <chenhao.lcch@gmail.com>
2015-03-02 11:24:28 -0800

    new load data

--- db.py
+++ db.py
@@ -1,6 +1,3 @@
-def remove_stopping_word(st):
-    stopwords = ['a', 'an', 'is', 'are', 'the', 'of', 'at', 'in', 'on', 'out', 'with', 'one', 'for', 'and', 'or']
-    return ' '.join([w for w in st.lower().split() if w not in stopwords])
 
 
 class Author(object):
@@ -12,7 +9,7 @@ class Author(object):
         self.candidate_papers = []
 
     def __str__(self):
-        return ', '.join([self.id, self.name, self.affiliation, str(self.num_candidate_papers)])
+        return ', '.join([str(self.id), self.name, self.affiliation, str(self.num_candidate_papers)])
 
     def add_candidate_paper(self, paper_id):
         self.candidate_papers.append(paper_id)
@@ -22,14 +19,14 @@ class Author(object):
         return self.name.split(' ')[-1]
 
     @staticmethod
-    def get_last_name(name):
+    def get_last_name_static(name):
         return name.split(' ')[-1]
 
 
 class Paper(object):
     def __init__(self, id, title, year, conference_id, journal_id, keywords):
         self.id = id
-        self.title = remove_stopping_word(title)
+        self.title = title
         self.year = year
         self.conference_id = conference_id
         self.journal_id = journal_id
@@ -38,8 +35,9 @@ class Paper(object):
         self.num_candidate_authors = 0
 
     def __str__(self):
-        return ', '.join([self.id, self.title, self.year,
-                          self.conference_id, self.journal_id, self.keywords,
+        return ', '.join([str(self.id), self.title, str(self.year),
+                          str(self.conference_id), str(self.journal_id), 
+                          str(self.keywords),
                           str(self.num_candidate_authors)])
 
     def add_candidate_author(self, author_info):
@@ -47,13 +45,3 @@ class Paper(object):
         self.candidate_authors.append(author_info)
         self.num_candidate_authors += 1
 
-
-class PaperAuthor(object):
-    def __init__(self, paper_id, author_id, name, affiliation):
-        self.paper_id = paper_id
-        self.author_id = author_id
-        self.name = name
-        self.affiliation = affiliation
-
-    def __str__(self):
-        return ', '.join([self.paper_id, self.author_id, self.name, self.affiliation])
--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ import csv
 import jellyfish
 from db import Author
 from db import Paper
-from db import PaperAuthor
 
+stoplist = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
 
 class Utils(object):
     def __init__(self):
@@ -15,15 +15,6 @@ class Utils(object):
         self.paper_list = []
         self.paper_dict = dict()
 
-        # Load databases: Author, Paper, PaperAuthor.
-        self.load_author_data('./dataRev2/Author.csv')
-        print("Load author data done...")
-
-        self.load_paper_data('./dataRev2/Paper.csv')
-        print("Load paper data done...")
-
-        self.load_paper_author_data('./dataRev2/PaperAuthor.csv')
-        print("Load paper-author data done...")
 
     # Given author id, returns Author instance reference
     def _author_id2author(self, author_id):
@@ -48,8 +39,16 @@ class Utils(object):
             reader = csv.reader(f)
             for row in reader:
                 if row[0] != 'Id':
-                    self.author_list.append(Author(row[0].lower(), row[1].lower(), row[2].lower()))
-                    self.author_dict[int(row[0])] = array_index
+                    author_id = int(row[0])
+                    name = row[1].lower()
+                    affiliation = row[2].lower()
+                    if len(affiliation) > 0:
+                        if affiliation[0] == '"' and affiliation[-1] == '"':
+                            affiliation = affiliation[1:-1]
+                    affiliation = " ".join([w for w in affiliation.replace(",",' ').replace(";", ' ').split() if w not in stoplist])
+                
+                    self.author_list.append(Author(author_id, name, affiliation))
+                    self.author_dict[author_id] = array_index
                     array_index += 1
 
     # Load Paper database
@@ -59,9 +58,26 @@ class Utils(object):
             reader = csv.reader(f)
             for row in reader:
                 if row[0] != 'Id':
-                    self.paper_list.append(Paper(row[0].lower(), row[1].lower(), row[2].lower(),
-                                                 row[3].lower(), row[4].lower(), row[5].lower()))
-                    self.paper_dict[int(row[0])] = array_index
+                    paper_id = int(row[0])
+
+                    title = row[1].lower()
+                    if len(title) > 0:
+                        if title[0] == '"' and title[-1] == '"':
+                            title = title[1:-1]
+                        title = " ".join([w for w in title.split() if w not in stoplist])
+
+                    year = int(row[2])
+                    if year <= 1000 or year > 2015:
+                        year = 0
+                    
+                    conference_id = int(row[3])
+                    journal_id = int(row[4])
+                    keywords = row[5]
+                    keywords = [w for w in keywords.lower().replace("|", ' ').replace(',', ' ').split()]
+
+                    self.paper_list.append(Paper(paper_id, title, year, conference_id, 
+                                                 journal_id, keywords))
+                    self.paper_dict[paper_id] = array_index
                     array_index += 1
 
     # Load PaperAuthor database
@@ -72,32 +88,25 @@ class Utils(object):
                 if row[0] != 'PaperId':
                     paper_id = int(row[0])
                     author_id = int(row[1])
+                    if paper_id not in self.paper_dict:
+                        continue
+
+                    name = row[2].lower()
+                    affiliation = row[3].lower()
+                    if len(affiliation) > 0:
+                        if affiliation[0] == '"' and affiliation[-1] == '"':
+                            affiliation = affiliation[1:-1]
+                        affiliation = " ".join([w for w in affiliation.replace(",",' ').replace(";", ' ').split() if w not in stoplist])
 
                     # TODO(@haochen): Now when the paper is not in the paper_list, we ignore it.
-                    paper_array_index = self.paper_dict.get(paper_id)
-                    if paper_array_index is None:
+                    if paper_id not in self.paper_dict:
                         continue
+                    paper = self.paper_list[self.paper_dict[paper_id]]
+                    paper.add_candidate_author((author_id, name, affiliation))
 
-                    self.paper_list[paper_array_index].add_candidate_author((row[1].lower(),
-                                                                             row[2].lower(),
-                                                                             row[3].lower()))
-
-                    author_array_index = self.author_dict.get(author_id)
-                    if author_array_index is not None:
-                        self.author_list[author_array_index].add_candidate_paper(paper_id)
-                        continue
-
-                    """
-                    if author_array_index is not None:
-                        self.author_list[author_array_index].add_candidate_paper(paper_id)
-                    else:
-                        self.author_dict[author_id] = len(self.author_list)
-                        self.author_list.append(Author(row[1], row[2], row[3]))
-                        self.author_list[-1].add_candidate_paper(paper_id)
-
-                    if paper_array_index is not None:
-                        self.paper_list[paper_array_index].add_candidate_author(author_id)
-                    """
+                    if author_id in self.author_dict:
+                        author = self.author_list[self.author_dict[author_id]]
+                        author.add_candidate_paper(paper_id)
 
     def get_all_last_name_of_authors(self, paper_id):
         paper = self._paper_id2paper(paper_id)
@@ -120,7 +129,7 @@ class Utils(object):
                 print(author)
                 self.find_non_ambiguous_papers(author_id)
 
-    def test_paper(self):g
+    def test_paper(self):
         while 1:
             paper_id = int(input('Paper Id: '))
             paper = self._paper_id2paper(paper_id)
@@ -150,6 +159,14 @@ class Utils(object):
 
                     wc = input("input any key to continue")
 
+    def features_author_paper(self, author_id, paper_id):
+        feature_vector = []
+        feature_vector.extend(self.features_author_profile(author_id, paper_id))
+        feature_vector.extend(self.features_coauthor_name_matching(author_id, paper_id))
+        feature_vector.extend(self.feature_time(author_id, paper_id))
+        feature_vector.extend(self.feature_bibliographic_network(author_id, paper_id))
+        return feature_vector
+
     def test_paper_author(self):
         while 1:
             author_id = int(input("Author Id: "))
@@ -170,13 +187,23 @@ class Utils(object):
 
             print("features_author_profile: ", self.features_author_profile(author_id, paper_id))
             print("features_coauthor_name_matching: ", self.features_coauthor_name_matching(author_id, paper_id))
-            print("\n")
+            print("features_year: ", self.feature_time(author_id, paper_id))
+            print("features_bibliographic network: ", self.feature_bibliographic_network(author_id, paper_id))
+
+
+            feature_vector = []
+            feature_vector.extend(self.features_author_profile(author_id, paper_id))
+            feature_vector.extend(self.features_coauthor_name_matching(author_id, paper_id))
+            feature_vector.extend(self.feature_time(author_id, paper_id))
+            feature_vector.extend(self.feature_bibliographic_network(author_id, paper_id))
+            print("OVER ALL: ", feature_vector)
 
     def features_author_profile(self, author_id, paper_id):
         # the levenshtein distance between the names of the target author in Author.csv
         # and PaperAuthor.csv
         author = self._author_id2author(author_id)
         paper = self._paper_id2paper(paper_id)
+
         author_name = author.name
         author_affiliation = author.affiliation
 
@@ -187,7 +214,7 @@ class Utils(object):
             if int(uid) == int(author_id):
                 continue
 
-            print("candidate: ", name, " : ", affiliation)
+            # print("candidate: ", name, " : ", affiliation)
             if len(name) > 0:
                 name_distance.append(jellyfish.levenshtein_distance(author_name, name))
             if len(author_affiliation) > 0 and len(affiliation) > 0:
@@ -204,36 +231,127 @@ class Utils(object):
         author_last_name = author.get_last_name()
         author_affiliation = author.affiliation
 
-        coauthor_name_dist = []
-        coauthor_last_name_dist = []
-        coauthor_name_dist_ig = []
-        coauthor_last_name_dist_ig = []
+        coauthor_name_jaro_dist = []
+        coauthor_lastname_jaro_dist = []
+        coauthor_name_jaro_dist_ig = []
+        coauthor_lastname_leve_dist_ig = []
 
-        coauthor_affiliation_jaro_dist = []
-        coauthor_affiliation_leve_dist = []
+        coauthor_affi_jaro_dist = []
+        coauthor_affi_leve_dist = []
 
         for uid, name, affiliation in paper.candidate_authors:
             if int(uid) == int(author_id):
                 continue
 
-            print("candidate: ", name, " : ", affiliation)
+            # print("candidate: ", name, " : ", affiliation)
             if len(name) > 0:
-                coauthor_name_dist.append(jellyfish.jaro_distance(author_name, name))
-                coauthor_last_name_dist.append(jellyfish.jaro_distance(author_last_name,
-                                                                       Author.get_last_name(name)))
-            if len(affiliation) > 0 and len(author_affiliation) > 0 and affiliation != author_affiliation:
-                coauthor_name_dist_ig.append(jellyfish.jaro_distance(author_name, name))
-                coauthor_last_name_dist_ig.append(jellyfish.levenshtein_distance(author_last_name,
-                                                                                 Author.get_last_name(name)))
+                coauthor_name_jaro_dist.append(jellyfish.jaro_distance(author_name, name))
+                coauthor_lastname_jaro_dist.append(
+                    jellyfish.jaro_distance(author_last_name, Author.get_last_name_static(name)))
 
-        return [max(coauthor_name_dist) if len(coauthor_name_dist) > 0 else 0.5,
-                max(coauthor_last_name_dist) if len(coauthor_last_name_dist) > 0 else 0.5,
-                max(coauthor_name_dist_ig) if len(coauthor_name_dist_ig) > 0 else 0.5,
-                min(coauthor_last_name_dist_ig) if len(coauthor_last_name_dist_ig) > 0 else 0.5]
+                if len(author_affiliation) > 0 and len(affiliation) > 0 and author_affiliation != affiliation:
+                    coauthor_name_jaro_dist_ig.append(jellyfish.jaro_distance(author_name, name))
+                    coauthor_lastname_leve_dist_ig.append(
+                        jellyfish.levenshtein_distance(author_last_name, Author.get_last_name_static(name)))
+
+            if len(author_affiliation) > 0 and len(affiliation) > 0:
+                coauthor_affi_jaro_dist.append(jellyfish.jaro_distance(author_affiliation, affiliation))
+                coauthor_affi_leve_dist.append(jellyfish.levenshtein_distance(author_affiliation, affiliation))
+
+        return [max(coauthor_name_jaro_dist) if len(coauthor_name_jaro_dist) > 0 else 0.5,
+                max(coauthor_lastname_jaro_dist) if len(coauthor_lastname_jaro_dist) > 0 else 0.5,
+                max(coauthor_name_jaro_dist_ig) if len(coauthor_name_jaro_dist_ig) > 0 else 0.5,
+                min(coauthor_lastname_leve_dist_ig) if len(coauthor_lastname_leve_dist_ig) > 0 else 0.5,
+                max(coauthor_affi_jaro_dist) if len(coauthor_affi_jaro_dist) > 0 else 0.5,
+                min(coauthor_affi_leve_dist) if len(coauthor_affi_leve_dist) > 0 else 0.5]
+
+    def feature_bibliographic_network(self, author_id, paper_id):
+        author = self._author_id2author(author_id)
+        paper = self._paper_id2paper(paper_id)
+
+        coauthors_id = []
+        for uid, name, affiliation in paper.candidate_authors:
+            if int(uid) == int(author_id):
+                continue
+            coauthors_id.append(int(uid))
+        number_coauthors = len(set(coauthors_id))
+
+        num_conference = 0
+        num_journal = 0
+        for pid in author.candidate_papers:
+            paper_related = self._paper_id2paper(int(pid))
+            if len(paper_related.conference_id) > 0 and paper_related.conference_id == paper.conference_id:
+                num_conference += 1
+            if len(paper_related.journal_id) > 0 and paper_related.journal_id == paper.journal_id:
+                num_journal += 1
+
+        return [author.num_candidate_papers, number_coauthors, num_conference, num_journal]
+
+    def feature_time(self, author_id, paper_id):
+        author = self._author_id2author(author_id)
+        paper = self._paper_id2paper(paper_id)
+
+        earliest_year = 2100
+        latest_year = 0
+        for pid in author.candidate_papers:
+            paper_related = self._paper_id2paper(int(pid))
+            if len(paper_related.year) > 2:
+                earliest_year = min(int(paper_related.year), earliest_year)
+                latest_year = max(int(paper_related.year), latest_year)
+
+        publication_year = int(paper.year) if len(paper.year) > 2 else 0
+        indicator_missing_year = 1 if publication_year > 0 else 0
+
+        return [earliest_year, latest_year, publication_year, indicator_missing_year]
+
+
+class FeatureGenerator:
+    def __init__(self):
+        self.utils = Utils()
+        # self.test()
+
+    def convert_train_data(self, filename):
+        data_list = []
+
+        count = 0
+        with open(filename, newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[0] != 'AuthorId':
+                    count += 1
+                    print("count", count)
+                    author_id = int(row[0])
+                    for pid in row[1].split(' '):
+                        item = self.utils.features_author_paper(author_id, int(pid))
+                        item.append(1)
+                        data_list.append(item)
+
+                    for pid in row[2].split(' '):
+                        item = self.utils.features_author_paper(author_id, int(pid))
+                        item.append(-1)
+                        data_list.append(item)
+
+        with open('features_train.csv', 'w', newline='') as f:
+            writer = csv.writer(f, delimiter=',')
+            for data in data_list:
+                writer.writerow(data)
 
 if __name__ == "__main__":
     utils = Utils()
+    
+    utils.load_author_data('./Cleaned/Author.csv')
+    print("Load author data done...")
+
+    utils.load_paper_data('./Cleaned/Paper.csv')
+    print("Load paper data done...")
+
+    utils.load_paper_author_data('./Cleaned/PaperAuthor.csv')
+    print("Load paper-author data done...")
+
     utils.test_paper_author()
+
+    # feature_generator = FeatureGenerator()
+    # feature_generator.convert_train_data('./dataRev2/Train.csv')
 
     #utils.test_dictionary()
     # utils.write_non_ambiguous_paper()
