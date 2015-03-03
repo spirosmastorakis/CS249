978a5a3cd345043bc3851e65cc69592c5a05035c
Spyridon Mastorakis <spiros.mastorakis@gmail.com>
2015-03-02 21:54:13 -0800

    Update deep learning network.

--- model.py
+++ model.py
@@ -5,16 +5,29 @@ import gensim, logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # input file
-sentences = []
+# sentences = [ ["athens"], ["test"], ["test", "data"], ["data"]]
 
 sentences = gensim.models.word2vec.LineSentence('PaperAuthor.csv')
 
-# convert to "multiword" sentences
-bigram_transformer = gensim.models.Phrases(sentences)
-
-# train word2vec on the two sentences
-model = gensim.models.Word2Vec(bigram_transformer[sentences], size = 200, min_count = 1, workers = 4)
+# train word2vec
+model = gensim.models.Word2Vec(sentences, window = 1, size = 100, min_count = 5, workers = 4, iter = 5)
 
 fname = "trained_model.txt"
+
 # store the trained model
 model.save(fname)
+
+# save the model for each 5 epochs
+
+for i in range(1,20):
+    # load stored model
+    model = gensim.models.Word2Vec.load(fname)
+
+    # train word2vec
+    model = gensim.models.Word2Vec(sentences, window = 1, size = 100, min_count = 5, workers = 4, iter = 5)
+
+    # filename to save model
+    fname = "trained_model" + "_" + str(i*5+5) + ".txt"
+
+    # store the trained model
+    model.save(fname)
