0ea2c8eadf47a6ceb5922388d1b14ebbab5acfcf
yss4 <yss4@cs.ucla.edu>
2015-03-04 00:26:42 -0800

    Add file to translate target.csv file
    Translate Target.csv (Valid.csv or Test.csv) into output file (authorid,
    paperid pairs csv format file)

---
+++ translate.py
@@ -0,0 +1,32 @@
+import csv,sys
+
+def translate_input(path):
+    with open(path[0], "r") as fi, open(path[1], 'w') as fo:
+        reader = csv.reader(fi)
+        next(fi)
+        fo.write(': data' + '\n')
+        for row in reader:
+            #print row[0]
+            paperid_list = row[1].split(" ")
+            pair_list = [[int(row[0]), int(elem)] for elem in paperid_list]
+
+            transformed_list = list(sum(pair_list, []))            
+            
+            for i,val in enumerate(transformed_list):
+                # print val
+                if i%2 == 1:
+                    fo.write(str(val) + '\n')
+                else:
+                    fo.write(str(val) + ' ')
+            
+
+def main(argv):
+    if len(argv) != 1 + 2:
+        print >> sys.stderr, 'Usage : %s Target.csv Transformed.csv' % (argv[0],)
+        return -1
+    
+    translate_input((argv[1],argv[2]))
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
